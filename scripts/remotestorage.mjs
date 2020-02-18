fd7ef0e253968e5561a7742420436b2385aeac1d
justgook <justgook@gmail.com>
2020-02-18 15:05:15 +0000

    Deploying to gh-pages from master - 8e7fc8247b7b5af7ddd3a48dba7efc285e924283 🚀

--- scripts/remotestorage.mjs
+++ scripts/remotestorage.mjs
@@ -2,27 +2,22 @@ import {loadScript} from "./util.mjs";
 
 
 export function initRemoteStorage() {
-    return loadScript("https://unpkg.com/remotestoragejs@1.2.2/release/remotestorage.js")
+    return loadScript("https://unpkg.com/remotestoragejs@1.2.3/release/remotestorage.js")
         .then(() => {
             const dataPath = "web-tiled";
             const remoteStorage = new RemoteStorage({ modules: [WebTiled] });
+            remoteStorage.setSyncInterval(60000);
+            // remoteStorage.enableLog();
             remoteStorage.access.claim(dataPath, "rw");
             remoteStorage.caching.enable(`/${dataPath}/`);
+            // https://remotestoragejs.readthedocs.io/en/latest/js-api/remotestorage.html#constructor
             remoteStorage.api = remoteStorage["web-tiled"];
+            remoteStorage.api.rpc = remoteStorage.api.rpc.bind(remoteStorage);
+
+
             return remoteStorage;
         });
 }
-
-export function createWidget(remoteStorage) {
-    loadScript("https://unpkg.com/remotestorage-widget@1.4.0/build/widget.js").then(() => {
-        const widget = new Widget(remoteStorage);
-        widget.attach();
-        const widgetElement = document.getElementById("remotestorage-widget")
-        widgetElement.style.bottom = 0;
-        widgetElement.style.right = 0;
-    });
-}
-
 
 const WebTiled = {
     name: "web-tiled",
@@ -79,12 +74,28 @@ const WebTiled = {
         //     console.log('publicClient::change', evt);
         // });
 
-        function rpc(request, response) {
-            request(({ method, params, id }) => {
-                exports[method].apply(null, params)
+        function rpc(subscribe, send) {
+            this.on("ready", (data) => send({ id: "ready", data }));
+            this.on("not-connected", (data) => send({ id: "not-connected", data }));
+            this.on("connected", () => send({ id: "connected", data: this.remote }));
+            this.on("disconnected", (data) => send({ id: "disconnected", data }));
+            this.on("error", (data) => send({ id: "error", data }));
+            this.on("features-loaded", (data) => send({ id: "features-loaded", data }));
+            this.on("connecting", (data) => send({ id: "connecting", data }));
+            this.on("authing", (data) => send({ id: "authing", data }));
+            this.on("wire-busy", (data) => send({ id: "wire-busy", data }));
+            this.on("wire-done", (data) => send({ id: "wire-done", data }));
+            this.on("sync-req-done", (data) => send({ id: "sync-req-done", data }));
+            this.on("sync-done", (data) => send({ id: "sync-done", data }));
+            this.on("network-offline", (data) => send({ id: "network-offline", data }));
+            this.on("network-online", (data) => send({ id: "network-online", data }));
+            this.on("sync-interval-change", (data) => send({ id: "sync-interval-change", data }));
+            exports.connect = (data) => Promise.resolve(this.connect(data));
+            exports.disconnect = (data) => Promise.resolve(this.disconnect(data));
+            subscribe(({ method, params, id }) => {
+                exports[method].apply(this, params)
                     .then((data) => {
-                        // console.log(method, params, id, data);
-                        response({ id, data })
+                        send({ id, data })
                     })
             });
         }
